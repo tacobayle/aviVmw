2e46efcc2d2a65c642a66bcb9dda18ed49b83f8f
Nicolas Bayle <nicolas.bayle.it@gmail.com>
2020-11-29 18:24:29 +0000

    20201129

--- variables.tf
+++ variables.tf
@@ -70,7 +70,7 @@ variable "ansible" {
     aviPbAbsentUrl = "https://github.com/tacobayle/ansiblePbAviAbsent"
     aviPbAbsentTag = "v1.36"
     aviConfigureUrl = "https://github.com/tacobayle/aviConfigure"
-    aviConfigureTag = "v3.0"
+    aviConfigureTag = "v3.01"
     version = "2.9.12"
     opencartInstallUrl = "https://github.com/tacobayle/ansibleOpencartInstall"
     opencartInstallTag = "v1.19"
